9a37e1a45e68b29fd97838658e996d4fc60e198f
dkoenig <Daniel.Koenig@it-novum.com>
2018-01-16 16:07:36 +0100

    adding env file and starting point for tomcat

--- repository/setEnvironment.sh
+++ repository/setEnvironment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## Adding JAVA_OPTS
-echo 'JAVA_OPTS="-XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Djava.awt.headless=true -XX:ReservedCodeCacheSize=128m $JAVA_OPTS"' >> /etc/default/tomcat7
-echo 'JAVA_OPTS="-Xms512M -Xmx2048M $JAVA_OPTS"' >> /etc/default/tomcat7
+echo 'JAVA_OPTS="-XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Djava.awt.headless=true -XX:ReservedCodeCacheSize=128m ${JAVA_OPTS}"' >> /etc/default/tomcat7
+echo 'JAVA_OPTS=" ${JAVA_OPTS} -Xms512M -Xmx2048M "' >> /etc/default/tomcat7
 
 
 
