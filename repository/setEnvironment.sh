3513584807e306b76312829d8c8fef683025dccb
dkoenig <Daniel.Koenig@it-novum.com>
2018-01-16 17:43:23 +0100

    some changes on Dockerfile

--- repository/setEnvironment.sh
+++ repository/setEnvironment.sh
@@ -4,7 +4,4 @@
 echo 'JAVA_OPTS="-XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -Djava.awt.headless=true -XX:ReservedCodeCacheSize=128m ${JAVA_OPTS}"' >> /etc/default/tomcat7
 echo 'JAVA_OPTS=" ${JAVA_OPTS} -Xms512M -Xmx2048M "' >> /etc/default/tomcat7
 
-
-
-
-
+sed -i 's/^shared.loader=.*/shared.loader=${catalina.home}\/shared\/classes,${catalina.home}\/shared\/*.jar,${catalina.base}\/shared\/classes/' /etc/tomcat7/catalina.properties
